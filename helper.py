80c7f8984c5bd362d470fac0b43ebb29a394d241
sitharaam-prog <sitharaam@gmail.com>
2026-05-01 10:15:12 +0530

    Added a few more functions

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import datetime as datet
 from math import pi
 
 def draw_bar() :
@@ -36,3 +37,44 @@ def draw_pie() :
 def find_area_of_cylinder(radius_base, height):
     area = ( 2 * pi * (radius_base ** 2) ) + ( 2 * pi * radius_base * height)
     return area   
+
+def date_time_functions() :  
+    todays_date = datet.datetime.now()
+    return todays_date
+
+def temperature_conversion_program(temperture, convert_to) :
+    if(convert_to == "F") :
+       temp = (9 * temperture)/5 + 32
+       return f"The temperature in fahrenheit is {temp: .2f}" 
+    elif (convert_to == "C") :
+          temp = (temperture - 32)*5/9
+          return f"The temperature in celsius is {temp: .2f}"
+    else :
+        return "You entered an invalid convert"    
+
+def factorial(num) :
+    fact = 1
+    step = 1
+    while step <= num :
+        fact = fact * step
+        step = step + 1
+    return fact   
+
+def find_largest_of_n_numbers(n):
+    step = 1
+    greatestnumber = 0
+    while step <= n :
+       num = int(input(f"Enter number {step}"))
+       if(greatestnumber < num) :
+         greatestnumber = num
+       step = step + 1 
+    return greatestnumber
+
+def input_numbers_into_an_array() :
+    arr = list(map(int, input("Enter numbers separated by space: ").split()))
+    largest_number_in_array = max(arr)
+    smallest_number_in_array = min(arr)
+    return largest_number_in_array, smallest_number_in_array 
+
+
+
--- main.py
+++ main.py
@@ -2,9 +2,31 @@ import helper as hlp
 
 print("Hello World")
 print("This is the data for today")
+print("First of all here is today's date in various formats")
+td = hlp.date_time_functions()
+print(td.strftime("%Y-%m-%d"))     
+print(td.strftime("%d/%m/%Y"))     
+print(td.strftime("%A, %B %d"))    
+print(td.strftime("%I:%M %p"))    
 
+print(f"{td: %Y-%m-%d}")
 area_cylind = hlp.find_area_of_cylinder(3, 4)
 
+print("Let us convert some temperatures")
+converted_temperature = hlp.temperature_conversion_program(32, "F")
+print(converted_temperature)
+converted_temperature = hlp.temperature_conversion_program(180, "C")
+print(converted_temperature)
+converted_temperature = hlp.temperature_conversion_program(180, "X")
+print(converted_temperature)
+
 print(f"area of cylinder today is {area_cylind:.2f}")
-hlp.draw_bar()
-hlp.draw_pie()
+factorial = hlp.factorial(0)
+print(f"{factorial}")
+largestnumber = hlp.find_largest_of_n_numbers(3)
+print(f"The largest number you entered is {largestnumber}")
+largestnumber_in_array_input, smallest_number_in_array_input = hlp.input_numbers_into_an_array()
+print(f"The largest number you entered is {largestnumber_in_array_input} and the smallest is {smallest_number_in_array_input}")
+
+#hlp.draw_bar()
+#hlp.draw_pie()
